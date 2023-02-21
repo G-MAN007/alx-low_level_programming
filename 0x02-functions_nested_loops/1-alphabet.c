94d86dbeebcb26849439c15430291fd1ee455965
G-MAN007 <nloyiso8@gmail.com>
2023-02-21 14:25:38 -0800

    Task 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 
 /**
 * main - Entry point
-*Description: prints the alphabet, in lowercase, followed by a new line.
+* Description: prints the alphabet, in lowercase, followed by a new line.
 * Return: void
-*/
+**/
 
 void print_alphabet(void)
 {
