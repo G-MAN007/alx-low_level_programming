208aebc7d2a03eef72aa2f60a147ebc4d8e4bba8
G-MAN007 <nloyiso8@gmail.com>
2023-02-21 16:10:53 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - Adds 2 values
+ *@n1: The first value to be comuted
+ *@n2: The second value to be computed
+ *Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
