03a4413a8edb54b78ad30101467d48badbb8268a
G-MAN007 <nloyiso8@gmail.com>
2023-02-22 16:16:02 -0800

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,9 +2,10 @@
 
 /**
  *print_times_table - Prints the times table of the input,
- *starting with 0
- *@n: The value of the times table to be printed
- *Return: n
+ *
+ * starting with 0.
+ *
+ * @n: The value of the times table to be printed.
  */
 
 void print_times_table(int n)
